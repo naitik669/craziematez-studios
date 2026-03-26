68c6a2fe585422cd3c0f863c38313c23eceeed38
naitik669 <naitik.business69@gmail.com>
2026-03-26 20:27:46 +0530

    Create RevisionModal.jsx

---
+++ features/components/RevisionModal.jsx
@@ -0,0 +1,10 @@
+export default function RevisionModal() {
+  return (
+    <div id="revisionModal" className="modal">
+      <div className="modal-content">
+        <h2>Revisions</h2>
+        <p>All revision history will appear here.</p>
+      </div>
+    </div>
+  );
+}
