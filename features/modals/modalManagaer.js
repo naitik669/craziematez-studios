883d32ad1c7f246204f275699e72944e4c0e5ae7
naitik669 <naitik.business69@gmail.com>
2026-03-26 20:29:38 +0530

    Delete features directory

--- features/modals/modalManagaer.js
+++
@@ -1,7 +0,0 @@
-export const openModal = (id) => {
-  document.getElementById(id).classList.add("active");
-};
-
-export const closeModal = (id) => {
-  document.getElementById(id).classList.remove("active");
-};
